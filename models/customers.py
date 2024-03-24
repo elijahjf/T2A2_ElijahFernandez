a91b2421fd9c417ff4f7c200ce281f859d6b53a0
Elijah <122788521+elijahjf@users.noreply.github.com>
2024-03-25 04:32:12 +1100

    created customers model

---
+++ models/customers.py
@@ -0,0 +1,12 @@
+from init import db, ma
+
+class Customers(db.Model):
+    __tablename__ = "customers"
+
+    customer_id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String)
+    customer_contact_number = db.Column(db.String)
+
+class CustomersSchema(ma.Schema):
+    class Meta:
+        fields = ("customer_id", "customer_name", "customer_contact_number")
